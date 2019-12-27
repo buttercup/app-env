441f42bfd5511f07bcfb65fcfdc3379bbde9d0ba
Perry Mitchell <perry@perrymitchell.net>
2019-12-27 15:53:41 +0200

    Add compression tools

--- source/native/index.js
+++ source/native/index.js
@@ -1,7 +1,9 @@
 const { getCryptoResources } = require("./crypto.js");
+const { getCompressionResources } = require("./compression.js");
 
 function applyNativeConfiguration(appEnv) {
     appEnv.setProperties(getCryptoResources());
+    appEnv.setProperties(getCompressionResources());
 }
 
 module.exports = {
