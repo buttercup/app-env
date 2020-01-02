2932ea3049a5fe01ec5a4440024baa00dab9c4f3
Perry Mitchell <perry@perrymitchell.net>
2020-01-02 21:06:13 +0200

    Add building for native lib

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,73 @@ const path = require("path");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 const SOURCE = path.resolve(__dirname, "./source");
-const OUTPUT = path.resolve(__dirname, "./web");
+const NATIVE_OUTPUT = path.resolve(__dirname, "./native");
+const WEB_OUTPUT = path.resolve(__dirname, "./web");
 
 const plugins = [];
 if (process.env.ANALYSE === "bundle") {
     plugins.push(new BundleAnalyzerPlugin());
 }
 
-module.exports = {
-    entry: path.join(SOURCE, "index.web.js"),
+const configs = [
+    {
+        entry: path.join(SOURCE, "index.web.js"),
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"]
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env"]
+                        }
                     }
                 }
-            }
-        ]
-    },
+            ]
+        },
 
-    node: {
-        crypto: "empty",
-        fs: "empty",
-        path: "empty"
-    },
+        node: {
+            crypto: "empty",
+            fs: "empty",
+            path: "empty"
+        },
 
-    output: {
-        filename: "index.js",
-        path: OUTPUT
-    },
+        output: {
+            filename: "index.js",
+            path: WEB_OUTPUT
+        },
 
-    plugins
-};
+        plugins
+    },
+    {
+        entry: path.join(SOURCE, "index.native.js"),
+
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env"]
+                        }
+                    }
+                }
+            ]
+        },
+
+        output: {
+            filename: "index.js",
+            path: NATIVE_OUTPUT
+        },
+
+        target: "node"
+    }
+];
+
+if (process.env.ANALYSE === "bundle") {
+    configs.splice(1, 1);
+}
+
+module.exports = configs;
